8e5f4a79d20ee7550cda57be2b915d9df98a6620
Yoel Baez <jbaezgis@gmail.com>
2022-05-31 15:21:55 -0400

    Mobile home page 1.0.8

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -46,7 +46,7 @@
                         <x-select
                             label="Passengers"
                             placeholder="Select passengers"
-                            clearable=false
+                            {{-- clearable --}}
                             icon="users"
                             :options="['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']"
                             wire:model="passengers"

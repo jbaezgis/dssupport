e6fbef62413e137e2380ea1a89f3877896771f12
Yoel Baez <jbaezgis@gmail.com>
2022-06-01 14:43:02 -0400

    Mobile form - 1.0.16

--- resources/views/livewire/booking-form.blade.php
+++ resources/views/livewire/booking-form.blade.php
@@ -27,8 +27,8 @@
 
                 
             </div>
-            <div class="flex justify-center gap-6 py-4">
-                <div class="text-center">
+            <div class="flex gap-6 py-4 justify-center">
+                <div class="">
                     <div class="text-sm text-gray-500">
                         @if ($booking->servicePrice->priceOption->id == 3)
                             {{-- <img class="mx-auto w-52" src="{{ asset('images/vehicles/Minivan.png') }}" alt=""> --}}
@@ -47,7 +47,7 @@
                     </div>
                 </div>
 
-                <div class="text-center">
+                <div class="">
                     <div class="text-sm text-gray-500">{{ __('Driving Time') }}</div>
                     {{-- {{formatDrivingTime($item->driving_time)}} --}}
                     <div class="">
@@ -60,7 +60,31 @@
                         @endif 
                     </div>
                 </div>
+
+                <div class="">
+                    <div class="text-sm text-gray-500">{{ __('Type') }}</div>
+                    {{-- {{formatDrivingTime($item->driving_time)}} --}}
+                    <div class="">
+                        @if ($booking->type == 'oneway')
+                            {{ __('One Way') }}
+                        @else
+                            {{ __('Round Trip') }}
+                        @endif
+                    </div>
+                </div>
+
+                <div class="">
+                    <div class="text-sm text-gray-500">{{ __('Order Total') }}</div>
+                    {{-- {{formatDrivingTime($item->driving_time)}} --}}
+                    <div class="">
+                        ${{ number_format($booking->order_total, 2, '.', ',') }}
+                    </div>
+                </div>
             </div>
+
+            {{-- <div class="flex justify-center gap-6 py-4">
+
+            </div> --}}
         </div>{{-- bg-white --}}
 
         {{-- Form --}}
@@ -83,7 +107,7 @@
                 <div class="mb-2">
                     <x-select
                         {{-- label="Select Status" --}}
-                        placeholder="Select language"
+                        placeholder="Preferred language"
                         wire:model="language"
                     >
                         <x-select.option label="English" value="en" />
@@ -95,7 +119,48 @@
                     {{ __('Arrival Information') }}
                 </div>
 
+                <div class="mb-2">
+                    <x-datetime-picker
+                        {{-- label="{{ __('To Date') }}" --}}
+                        placeholder="{{ __('Arrival date') }}"
+                        wire:model="toDate"
+                        without-time="true"
+                        parse-format="YYYY-MM-DD"
+                    />
+                </div>
+
+                <div class="mb-2">
+                    <x-time-picker
+                        placeholder="Arrival time"
+                        wire:model.defer="timePicker"
+                    />
+                </div>
+                <div class="mb-2">
+                    <x-input placeholder="Arrival airline" />
+                </div>
+                <div class="mb-2">
+                    <x-input placeholder="Arrival flight" />
+                </div>
+
+                <div class="mb-2">
+                    <div class="text-sm text-gray-600">
+                        {{ __('Please enter the name of your hotel or the address of your drop off location, as well as any additional information we should know.') }}
+                    </div>
+                    <x-textarea placeholder="More information" />
+                </div>
             </form>
         </div>
+
+        <div class="flex justify-center mt-4">
+            <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                <button type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-blue-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-blue-800 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                    {{__('Save and Continue')}}
+                </button>
+                {{-- <button wire:click.prevent="save()" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-blue-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-blue-800 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                    {{__('Save and Continue')}}
+                </button> --}}
+            </span>
+        </div>
+
     </div>{{-- main div --}}
 </div>
